be16b248cf97fcbe2abbb5ffc366c4e6c0d3775b
AlejandroNahuel <alejandro.nahuel15817@gmail.com>
2022-06-29 16:50:35 -0300

    some improvements added

--- js/main.js
+++ js/main.js
@@ -1,2 +1,38 @@
+//Main section, the one that has the canvas inside
+let $mainSection = document.getElementById('mainSection');
+
+//Offcanvas itself
+$offcanvas = document.getElementById('offcanvasResponsive')
+
+//Sections elements
+$home = document.getElementById('homeSection');
+$technicalSkills = document.getElementById('technicalSkillsSection')
+$projects = document.getElementById('projectsSection')
+
+//Elements' "y" position
+home_Y = $home.offsetTop;
+technicalSkills_Y = $technicalSkills.offsetTop;
+projects_Y = $projects.offsetTop;
+
+//Canvas links
+let $canvasHome = document.getElementById('canvasHome');
+let $canvasTechnicalSkills = document.getElementById('canvasTechnicalSkills');
+let $canvasProjects = document.getElementById('canvasProjects');
+
+//window scroll
+function scrollToElement(element){
+    window.scroll(0, element.offsetTop);
+}
+
+//EventListeners
+$canvasHome.addEventListener('click', e =>{
+    scrollToElement($home);
+});
+$canvasTechnicalSkills.addEventListener('click', e =>{
+    scrollToElement($technicalSkills);
+});
+$canvasProjects.addEventListener('click', e =>{
+    scrollToElement($projects);
+});
 
 
